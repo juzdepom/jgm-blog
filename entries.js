bfab1c9820ebe316d534159a019d0905515b7421
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-01-06 06:04:52 -0800

    initial commit. Jan3-Jan6.

---
+++ entries.js
@@ -0,0 +1,38 @@
+const entries = [{
+  id: 4,
+  date: "01/06/2018",
+  title: "Day 5 of Brainwashing Experiment",
+  priorities: ['create the JGM blog', 'jog/walk 10 miles'],
+  stream: "",
+  gratitude: ['Udemy courses', 'Discovery of the 5 second rule', 'Kostas for continuing to believe in me', 'Having two caring parents'],
+  mantra: "I am self disciplined.",
+},
+{
+  id: 3,
+  date: "01/05/2018",
+  title: "Day 4 of Brainwashing Experiment"
+  priorities: ['write out goals with Tony Robbins Unlimited Power book', 'jog/walk 10 miles'],
+  stream: "",
+  gratitude: ["Home made meals made by Mom", "Zero app for fasting", "all the sports bras Mom has bought me"],
+  mantra: "I am self disciplined."
+},
+{
+  id: 2,
+  date: "01/03/2018",
+  title: "Day 3 of Brainwashing Experiment.",
+  priorities: ['Jog 10 miles', 'update Dad and Alden on how things are going'],
+  stream: "",
+  gratitude: ["All the running jackets I want", "the washing machine", "a bathroom for myself where I can shower as many times as I want"],
+  mantra: "I am self disciplined."
+},
+{
+  id: 1,
+  title: "Day 2 of Brainwashing Experiment."
+  date: "01/03/2018",
+  priorities: [],
+  stream: "",
+  gratitude: [],
+  mantra: "I am self disciplined."
+
+},
+]
